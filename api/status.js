d957f7116c9dc2a4fa453812494233b5024b6752
rafalolszak <130851447+rafalolszak@users.noreply.github.com>
2026-04-10 20:56:19 +0200

    Update status.js

--- api/status.js
+++ api/status.js
@@ -1,8 +1,15 @@
 import Replicate from "replicate";
-const replicate = new Replicate({ auth: process.env.REPLICATE_API_TOKEN });
+
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_TOKEN,
+});
 
 export default async function handler(req, res) {
   const { id } = req.query;
-  const prediction = await replicate.predictions.get(id);
-  res.status(200).json(prediction);
+  try {
+    const prediction = await replicate.predictions.get(id);
+    res.status(200).json(prediction);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }

255f86dd08bb5775a86030b36f1e2e3f163f248b
rafalolszak <130851447+rafalolszak@users.noreply.github.com>
2026-04-11 12:37:37 +0200

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
           image_input: [
             "[https://formmedes.pl/wp-content/uploads/2022/11/implanty-twarzy-formmedes.jpg]"
           ],
-          prompt: prompt,
+          prompt: "znajdż włosy na zdjęciu i zmień je na długie",
         },
       }
     );

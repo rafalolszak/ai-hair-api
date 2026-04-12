e045b98562bb1328a99b7402cba3427aa75f1766
rafalolszak <130851447+rafalolszak@users.noreply.github.com>
2026-04-12 22:20:54 +0200

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -33,7 +33,7 @@ export default async function handler(req, res) {
       .filter(key => hairDictionary[key]) // bierzemy tylko te, które mamy w słowniku
       .map(key => ({
         label: key,
-        fullPrompt: `${hairDictionary[key]}, ${details}, highly detailed, realistic, 8k, nie zmieniaj pozycji twarzy`
+        fullPrompt: `${hairDictionary[key]}, ${details}, nie zmieniaj pozycji twarzy`
       }));
   }
 
